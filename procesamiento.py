1864685f484306283f6d476ab8fc7fb2eab24048
Physical8 <leomaje@gmail.com>
2024-04-08 14:59:55 -0500

    Cruce de Tablas, MF encontrado pero elimina valores no encontrados

--- procesamiento.py
+++ procesamiento.py
@@ -4,10 +4,36 @@ df_TablaSoluING = pd.DataFrame()
 flypasID_df = pd.DataFrame()
 
 def procesar_archivos(flypass_df, descargue_df, fecha_inicio_df, fecha_fin_df):
+    global found_mfto_df
 #def procesar_archivos(flypass_df, general_df, descargue_df, trayectos_df, acumulado_df, fecha_inicio_df, fecha_fin_df):
-    #flypass_df = modificacion_flypass(flypass_df, fecha_inicio_df, fecha_fin_df)
+    flypass_df = modificacion_flypass(flypass_df, fecha_inicio_df, fecha_fin_df)
     descargue_df = modificacion_descargue(descargue_df)
-    df_TablaSoluING = descargue_df
+    #df_TablaSoluING = flypass_df
+    #df_TablaSoluING = descargue_df
+  
+    # Supongamos que tienes tus dos DataFrames df1 y df2
+
+    # Convertimos la columna 'FECHA_MVTO' a tipo datetime en df1
+    flypass_df['FECHA_MVTO'] = pd.to_datetime(flypass_df['FECHA_MVTO'])
+
+    # Realizamos la fusión de los DataFrames en función de la columna 'PLACA'
+    merged_df = pd.merge(flypass_df, descargue_df, on='PLACA', how='left')
+
+    # Filtramos las filas donde la 'FECHA_MVTO' está dentro del rango de 'CARGUE' y 'DESCARGUE'
+    filtered_df = merged_df[(merged_df['FECHA_MVTO'] >= merged_df['CARGUE']) & (merged_df['FECHA_MVTO'] <= merged_df['DESCARGUE'])]
+
+    # Creamos la columna 'MFTO ENCONTRADO' con los valores de 'MFTO' cuando la 'PLACA' coincide y la 'FECHA_MVTO' está dentro del rango
+    filtered_df.loc[:, 'MFTO ENCONTRADO'] = filtered_df['MFTO']
+
+    # Cuando la 'PLACA' coincide pero la 'FECHA_MVTO' no está dentro del rango, asignamos 'No encontrado'
+    filtered_df.loc[~filtered_df['MFTO ENCONTRADO'].notnull(), 'MFTO ENCONTRADO'] = 'No encontrado porque la FECHA_MVTO no está dentro del rango CARGUE y DESCARGUE de la placa correspondiente'
+
+    # Eliminamos registros duplicados basados en la columna 'PLACA'
+    df_TablaSoluING = filtered_df.drop_duplicates(subset=['ID'])
+
+    # Seleccionamos las columnas deseadas
+    df_TablaSoluING = df_TablaSoluING[['ID', 'FECHA_MVTO', 'PLACA', 'MFTO ENCONTRADO']]
+
     # Devolver el DataFrame modificado
     return df_TablaSoluING
 
@@ -78,3 +104,4 @@ def modificacion_descargue(descargue_df):
 
 
     return descargue_df
+
